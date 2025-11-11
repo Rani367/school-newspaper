dc79ffdfd660dbb864d2fca8eb21f7368973b49d
rani <rani2011367@gmail.com>
2025-11-11 11:10:16 +0200

    removed redundent files

--- dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Development server startup script with environment variables
-
-export POSTGRES_URL='postgres://localhost:5432/school_newspaper'
-export POSTGRES_URL_NON_POOLING='postgres://localhost:5432/school_newspaper'
-
-pnpm run dev
